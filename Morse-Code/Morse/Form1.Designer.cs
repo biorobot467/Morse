e15f6e7844f4db2ff2e16a0fd9bf43fae96f0f6a
biorobot467 <84491906+biorobot467@users.noreply.github.com>
2021-06-16 13:28:07 +0700

    Add files via upload

---
+++ Morse-Code/Morse/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace Morse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.ForeColor = System.Drawing.Color.Lime;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Russian",
+            "English"});
+            this.comboBox1.Location = new System.Drawing.Point(85, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(70, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.Color.Black;
+            this.comboBox2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox2.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.ForeColor = System.Drawing.Color.Lime;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "To Code",
+            "To Letter"});
+            this.comboBox2.Location = new System.Drawing.Point(85, 97);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(70, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(177, 133);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Перевод";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(102, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Язык";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(70, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "В код/Из кода";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(46, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Статус процесса:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Lime;
+            this.textBox1.Location = new System.Drawing.Point(198, 180);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(219, 13);
+            this.textBox1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.ForeColor = System.Drawing.Color.Lime;
+            this.textBox2.Location = new System.Drawing.Point(239, 58);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(178, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox3.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox3.ForeColor = System.Drawing.Color.Lime;
+            this.textBox3.Location = new System.Drawing.Point(239, 100);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(178, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(236, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(191, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Название входного файла";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(212, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(231, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Название файла с результатом";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Console", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.ForeColor = System.Drawing.Color.Lime;
+            this.label6.Location = new System.Drawing.Point(122, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(205, 21);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Кодировка Морзе";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Lucida Console", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            this.button2.Location = new System.Drawing.Point(177, 214);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Lucida Console", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.ForeColor = System.Drawing.Color.Lime;
+            this.label7.Location = new System.Drawing.Point(300, 228);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(145, 12);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "By Капустина Варвара";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(457, 249);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Morse-Code/Morse/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Morse
+{
+    public partial class Form1 : Form
+    {
+        private const string PATH = @"G:\С#\Morse-Code\";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sC = null,
+                rStr = null;
+            char bStr = '\0';
+            bool MesE = false;
+            int cP = 0;
+            string ps = PATH + textBox2.Text;
+            string pr = PATH + textBox3.Text;
+            string lge = "";
+            if (comboBox1.Text == "Russian") { lge = "r"; } else { lge = "e"; }
+
+            if (lge != "e" && lge != "r")
+            {
+            }
+            else
+            {
+                if (lge == "e") lge = "en";
+                if (lge == "r") lge = "rus";
+                MC mes = new MC(lge);
+                char a = '0';
+                if (comboBox2.SelectedIndex == 0) { a = 'l'; } else { a = 'c'; }
+                using (StreamWriter stW = new StreamWriter(pr, false, System.Text.Encoding.UTF8)) { }
+                using (StreamReader stR = File.OpenText(ps))
+                {
+                    while (stR.Peek() != -1)
+                    {
+                        MesE = true;
+                        if (a == 'c')
+                        {
+                            while (bStr != ' ')
+                            {
+                                bStr = (char)stR.Read();
+                                if (bStr == '\uffff')
+                                    break;
+                                if (bStr != ' ')
+                                    cP = 0;
+                                else
+                                    cP++;
+                                if (bStr != ' ')
+                                    sC += bStr;
+                            }
+                            if (sC != null)
+                                rStr = Convert.ToString(mes.Decode(sC));
+                            if (cP == 2)
+                                rStr = Convert.ToString(mes.Decode(Convert.ToString(bStr)));
+                            sC = null;
+                            bStr = '\0';
+                        }
+                        else if (a == 'l')
+                            rStr = mes.Code((char)stR.Read());
+                        else
+                            Console.WriteLine("You can choose only c or l!");
+                        using (StreamWriter stW = new StreamWriter(pr, true, System.Text.Encoding.UTF8))
+                            stW.Write(rStr);
+                    }
+                }
+                if (MesE == true)
+                    textBox1.Text = "Check the result file.";
+                else
+                    textBox1.Text = "File is empty!";
+            }
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+
+
+    public class MC
+    {
+        private const string PATH = @"G:\С#\Morse-Code\";
+        private string _lge;
+        private Dictionary<string, string> aD = new Dictionary<string, string> { };
+        private Dictionary<char, string> cD = new Dictionary<char, string> { };
+        public delegate void ErrorMessage(string tOfE);
+        public event ErrorMessage NE;
+
+        public MC(String lge)
+        {
+            _lge = lge;
+            switch (_lge)
+            {
+                case "en":
+                    CrDic(PATH+"EnglishDictionary.txt");
+                    break;
+                case "rus":
+                    CrDic(PATH+"RussianDictionary.txt");
+                    break;
+                default:
+                    Console.WriteLine("Language uncorrect!");
+                    break;
+            }
+        }
+private void CrDic(string p)
+        {
+            String mFF = null, c = null;
+            int ix = 2, lh;
+
+            using (StreamReader fS = File.OpenText(p))
+            {
+                while (mFF != "///")
+                {
+                    mFF = fS.ReadLine();
+                    if (mFF != "///")
+                    {
+                        lh = mFF.Length;
+
+                        while (ix < lh)
+                        {
+                            c += mFF[ix];
+                            ix++;
+                        }
+                        aD.Add(c, Convert.ToString(mFF[0]));
+                        cD.Add(mFF[0], c);
+                    }
+                    ix = 2;
+                    c = null;
+                }
+            }
+        }
+        private bool IsCode(char sl)
+        {
+            if (sl == '-' || sl == '.' || sl == ' ' || sl == '\n')
+                return true;
+            else
+                return false;
+        }
+        private bool IsLe(char sl)
+        {
+            if (_lge == "en")
+            {
+                if ((sl >= 'A' && sl <= 'Z') ||
+                    (sl >= 'a' && sl <= 'z') ||
+                    (sl >= '0' && sl <= '9') ||
+                    (sl == ' '))
+                    return true;
+            }
+            if (_lge == "rus")
+            {
+                if ((sl >= 'А' && sl <= 'Я') ||
+                    (sl >= 'а' && sl <= 'я') ||
+                    (sl >= '0' && sl <= '9') ||
+                    (sl == ' '))
+                    return true;
+            }
+            return false;
+        }
+
+        private char ChToU(char sl)
+        {
+            if ((sl >= 'a' && sl <= 'z') || (sl >= 'а' && sl <= 'я'))
+                return (char)(sl - ' ');
+            else
+                return sl;
+        }
+        public string Code(char sL)
+        {
+            sL = ChToU(sL);
+            string resultString = null;
+            if (IsLe(sL) == true)
+            {
+                try
+                {
+                    resultString = cD[sL];
+                    resultString += " ";
+
+                    return resultString;
+                }
+                catch (Exception)
+                {
+                    if (sL == ' ')
+                        return " ";
+
+                    return null;
+                }
+            }
+            else
+            {
+                return Convert.ToString(sL) + " ";
+            }
+        }
+        public string Decode(string sr)
+        {
+            if (IsCode(sr[0]) == true)
+            {
+
+                try
+                {
+                    return aD[sr];
+                }
+
+                catch (Exception)
+                {
+                    if (NE != null)
+                        NE("Invalid code!");
+                    else
+                        Console.WriteLine("Invalid code!");
+
+                    return sr;
+                }
+            }
+            else
+            {
+                return sr;
+            }
+        }
+    }
+}
+    
